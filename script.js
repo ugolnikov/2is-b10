cbc007d003e282f1781c8bc802ec3e72f2536aa9
ugolnikov <42218732+ugolnikov@users.noreply.github.com>
2024-02-12 15:15:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,27 @@ test.addEventListener(
         test.src="Jumpscare.png"
     },
     false,
-  );
+);
 test.addEventListener(
     "mouseout",
     (event) => {
         test.style.cssText = "height: 120px;";
         test.src="scale_1200.png"},
     false,
-  );
+);
+test.addEventListener(
+    "touchstart",
+    (event) => {
+        test.style.cssText = "height: 500px; width:500px";
+        test.src="Jumpscare.png"
+    },
+    false,
+);
+test.addEventListener(
+    "touchend",
+    (event) => {
+        test.style.cssText = "height: 120px;";
+        test.src="scale_1200.png"},
+    false,
+);
+
