ab249dac63c4b47516873d73e639b6cb27071f1c
ugolnikov <42218732+ugolnikov@users.noreply.github.com>
2024-02-14 13:13:46 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,10 @@
 <!doctype html>
- 2 <html lang="en-us">
- 3   <head>
- 4     <meta charset="utf-8">
- 5     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-11   </head>
-12   <body>
-13    <iframe seamless="seamless" allowtransparency="true" allowfullscreen="true" frameborder="0" style="width: 100%;height: 100%;border: 0px;" src="https://zv1y2i8p.play.gamezop.com/g/SkQwnwnbK"> </iframe>
-30 </body>
+ <html lang="en-us">
+ <head>
+ <meta charset="utf-8">
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<iframe seamless="seamless" allowtransparency="true" allowfullscreen="true" frameborder="0" style="width: 100%;height: 100%;border: 0px;" src="https://zv1y2i8p.play.gamezop.com/g/SkQwnwnbK"> </iframe>
+</body>
 </html>
