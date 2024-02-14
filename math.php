2f4dd342e0167d3b43efd17fc163171f24306b54
ugolnikov <42218732+ugolnikov@users.noreply.github.com>
2024-02-14 13:25:54 +0300

    Create math.php

---
+++ math.php
@@ -0,0 +1,6 @@
+
+<!-- iDev.Games Responsive Embed Code for Multi Bomb -->
+<div style="position: relative;height: 0;overflow: hidden;padding-bottom: 56.25%;">
+    <iframe id="embededGame" src="https://idev.games/embed/multi-bomb" scrolling="no" seamless="seamless" frameBorder="0" style="position: absolute;top:0;left: 0;width: 100%;height: 100%;">Browser not compatible.</iframe>
+</div>
+<!-- End Embed Code -->
