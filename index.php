c0cbc18377229f2be7337b60fb97b9f0a1469bac
Daniil Ugolnikov <starconflict.2005@mail.ru>
2024-02-25 20:40:58 +0500

    login system

--- index.php
+++ index.php
@@ -4,13 +4,27 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>СТАРТ | Школа плавания олимпийского резерва</title>
+  <link rel="icon" type="image/x-icon" href="/static/favicon.ico">
+  <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
-  Всем привет!
-  йо
-  всем привет
+  <div class="header">
+    <img src="/static/logo.png" alt="Logo" class="logo">
+    <ul>
+      <li><a href="/">Главная</a></li>
+      <li><a href="/about">О нас</a></li>
+      <li><a href="/news">Новости</a></li>
+      <li><a href="/contacts">Контакты</a></li>
+      <li><a href="/login">Личный кабинет</a></li>
+    </ul>
+  </div>
+
+  <div class="main-img">
+    <p>Мы "СТАРТ" и мы гордимся этим!</p>
+    <img src="/static/main.png" alt="Main picture">
+  </div>
 </body>
 
 </html>
---
+++ static/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', '2is-b10_2is-b10');
+define('DB_PASSWORD', 'HKqddAK447');
+define('DB_DATABASE', '2is-b10_2is-b10');
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
