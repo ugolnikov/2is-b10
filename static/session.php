63be87bd426b8028d9fb0b3206664c0f42741903
Daniil Ugolnikov <starconflict.2005@mail.ru>
2024-03-04 00:57:57 +0500

    ses

--- static/session.php
+++ static/session.php
@@ -4,18 +4,21 @@ session_start();
 
 $user_check = $_SESSION['login_user'];
 
-
 $admin_check = mysqli_query($db, "SELECT id FROM admin WHERE username = '$user_check'");
 $isAdmin = mysqli_num_rows($admin_check) > 0;
 
 if (!isset($_SESSION['login_user'])) {
-  header("location: ../login");
-  die();
+  if (!strpos($_SERVER['REQUEST_URI'], 'login')) {
+    header("location: ../login");
+    die();
+  }
 }
 
 if ($isAdmin) {
-  header("location: ../admin_dashboard");
-  die();
+  if (!strpos($_SERVER['REQUEST_URI'], 'admin_dashboard')) {
+    header("location: ../admin_dashboard");
+    die();
+  }
 }
 
 $ses_sql = mysqli_query($db, "SELECT username FROM users WHERE username = '$user_check'");
