5db268ba42b1b1b65ba935351d36fd9066dec5e1
Daniil Ugolnikov <starconflict.2005@mail.ru>
2024-03-06 02:13:21 +0500

    servasd

--- services/index.php
+++ services/index.php
@@ -92,24 +92,22 @@ include("../static/session.php");
     <!-- Услуги -->
     <h2>Выберите услугу:</h2>
     <form action="" method="post">
-        <div class="service">
-            <img src="service1.jpg" alt="Услуга 1">
-            <p>Услуга 1</p>
-            <input type="hidden" name="service_id" value="1">
-            <input type="submit" name="submit_service_1" value="Добавить в корзину">
-        </div>
-        <div class="service">
-            <img src="service2.jpg" alt="Услуга 2">
-            <p>Услуга 2</p>
-            <input type="hidden" name="service_id" value="2">
-            <input type="submit" name="submit_service_2" value="Добавить в корзину">
-        </div>
-        <div class="service">
-            <img src="service3.jpg" alt="Услуга 3">
-            <p>Услуга 3</p>
-            <input type="hidden" name="service_id" value="3">
-            <input type="submit" name="submit_service_3" value="Добавить в корзину">
-        </div>
+        <img src="service1.jpg" alt="Услуга 1">
+        <p>Услуга 1</p>
+        <input type="hidden" name="service_id" value="1">
+        <input type="submit" value="Добавить в корзину">
+    </form>
+    <form action="" method="post">
+        <img src="service2.jpg" alt="Услуга 2">
+        <p>Услуга 2</p>
+        <input type="hidden" name="service_id" value="2">
+        <input type="submit" value="Добавить в корзину">
+    </form>
+    <form action="" method="post">
+        <img src="service3.jpg" alt="Услуга 3">
+        <p>Услуга 3</p>
+        <input type="hidden" name="service_id" value="3">
+        <input type="submit" value="Добавить в корзину">
     </form>
 
     <?php
@@ -118,14 +116,7 @@ include("../static/session.php");
             include('../static/config.php');
 
             $user_id = $_SESSION['user_id'];
-
-            if (isset($_POST['submit_service_1'])) {
-                $service_id = $_POST['service_id'];
-            } elseif (isset($_POST['submit_service_2'])) {
-                $service_id = $_POST['service_id'];
-            } elseif (isset($_POST['submit_service_3'])) {
-                $service_id = $_POST['service_id'];
-            }
+            $service_id = $_POST['service_id'];
 
             $quantity = 1;
 
